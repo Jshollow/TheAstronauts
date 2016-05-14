f81b586c992804c2643cff449c07c69886763d85
Phillip Yuen <nobisnews@gmail.com>
2016-05-14 18:25:22 -0500

    hpd.css, hpd.js, index.html editted: add grid and banner

--- hpd.js
+++ hpd.js
@@ -14,3 +14,13 @@ month = (month * 1) + 1
 day = test.getDate()
 year = test.getFullYear()
 document.write(" ",month,"/",day,"/",year," ")
+
+var elementPosition = $('#navbar-title').offset();
+
+$(window).scroll(function(){
+        if($(window).scrollTop() > elementPosition.top){
+            $('#navbar-title').addClass('pastBanner')
+        } else {
+            $('#navbar-title').removeClass('pastBanner');
+        }    
+});
